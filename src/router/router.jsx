160bd41a714777e6151f49531be01ed8bcc9b24d
fahadbgnr <aminulislamfahad1@gmail.com>
2025-07-04 16:55:04 +0600

    project done

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -23,6 +23,7 @@ import RiderRoute from "../routes/RiderRoute";
 import PendingDeliveries from "../pages/Dashboard/PendingDeliveries/PendingDeliveries";
 import CompletedDeliveries from "../pages/Dashboard/CompletedDeliveries/CompletedDeliveries";
 import MyEarnings from "../pages/Dashboard/MyEarnings/MyEarnings";
+import DashboardHome from "../pages/Dashboard/DashboardHome/DashboardHome";
 
 export const router = createBrowserRouter([
     {
@@ -76,6 +77,10 @@ export const router = createBrowserRouter([
             <DashboardLayout></DashboardLayout>
         </PrivateRoute>,
         children: [
+            {
+                index: true,
+                Component: DashboardHome
+            },
             {
                 path: 'myParcels',
                 Component: MyParcels
